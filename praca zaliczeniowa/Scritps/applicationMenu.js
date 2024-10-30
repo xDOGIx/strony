75000038a11db62b4199d836f23002cbc7f5e91f
xDOGIx <144941805+xDOGIx@users.noreply.github.com>
2024-10-30 21:01:06 +0100

    Add files via upload

---
+++ praca zaliczeniowa/Scritps/applicationMenu.js
@@ -0,0 +1,7 @@
+const openMemu = () => {
+  console.log("Test wee dzila !!!");
+};
+
+memu.addEventListener("click", () => {
+  openMemu();
+});
---
+++ praca zaliczeniowa/Scritps/elements.js
@@ -0,0 +1,5 @@
+const memu = document.querySelector("#menu");
+
+memu.addEventListener("click", () => {
+  window.location.href = "https://www.gry.pl/gry/gry-kasynowe";
+});
